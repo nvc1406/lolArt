3a3b9a31e3b4cf805d645032a95409b80c4946ab
Ng?c Nam <namnn@novaon.vn>
2015-11-09 10:08:04 +0700

    New commit - push code

---
+++ LanguageOfLegendArt.Core/LanguageOfLegendArt.Core/LanguageOfLegendArt.Controller/Category/CategoryResponsitory.cs
@@ -0,0 +1,13 @@
+﻿using LanguageOfLegendArt.Core.UnitOfWork;
+
+namespace LanguageOfLegendArt.Core.LanguageOfLegendArt.Controller.Category
+{
+    class CategoryResponsitory: DataCenterInterfaceBase<Model.Category>
+    {
+        public CategoryResponsitory(IUnitOfWork unit)
+            : base(unit)
+        {
+            
+        }
+    }
+}
--- LanguageOfLegendArt.Core/LanguageOfLegendArt.Core/LanguageOfLegentArt.Constkey/EnumKey.cs
+++ LanguageOfLegendArt.Core/LanguageOfLegendArt.Core/LanguageOfLegentArt.Constkey/EnumKey.cs
@@ -14,5 +14,10 @@ namespace LanguageOfLegendArt.Core.LanguageOfLegentArt.Constkey
         public const int UpdateFail = 4;
         public const int RemoveSuccess = 5;
         public const int RemoveFail = 6;
+        public const int Running = 1000;
+        public const int Waiting = 1001;
+        public const int Block = 1002;
+        public const int Deleteted = 1003;
+
     }
 }
