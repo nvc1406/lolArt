812a5dcb787861777d314081561d78b96a749ddf
cuongnv <cuongnv@novaon.vn>
2015-11-09 09:38:16 +0700

    up code

---
+++ LanguageOfLegendArt/lolArt.web.admin/lolArt.web.admin/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace lolArt.web.admin
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ LanguageOfLegendArt/lolArt.web.admin/lolArt.web.admin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lolArt.web.admin.Startup))]
+namespace lolArt.web.admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
