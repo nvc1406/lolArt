f6a05d4ee80390f775d99c5ea92ec744fdfbe672
Ng?c Nam <namnn@novaon.vn>
2015-11-11 08:17:40 +0700

    New commit - ver 1

--- LanguageOfLegendArt/LoLArt.admin/Site.Master.designer.cs
+++ LanguageOfLegendArt/LoLArt.admin/Site.Master.designer.cs
@@ -13,12 +13,12 @@ namespace LoLArt.admin {
     public partial class SiteMaster {
         
         /// <summary>
-        /// MainContent control.
+        /// plcContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder plcContent;
     }
 }
--- LanguageOfLegendArt/lolArt.web.admin/lolArt.web.admin/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lolArt.web.admin.MvcApplication" Language="C#" %>
